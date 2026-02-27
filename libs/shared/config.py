fb5b480e2163d4ac759943864f91a3c4f75160a6
Nahom Wm <dbu1601542@dbu.edu.et>
2026-02-27 19:01:09 +0300

    Define Kafka topics for raw and processed legal data

--- libs/shared/config.py
+++ libs/shared/config.py
@@ -9,3 +9,8 @@ class Settings(BaseSettings):
     OPENAI_API_KEY: str = os.getenv('OPENAI_API_KEY', '')
 
 settings = Settings()
+class KafkaTopics:
+    RAW_LAWS = 'legal_raw'
+    PROCESSED_LAWS = 'legal_processed'
+kafka_topics = KafkaTopics()
+
