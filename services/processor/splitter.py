af4c029e0c270aee28cbd83e1488ae94893c43f2
Nahom Wm <dbu1601542@dbu.edu.et>
2026-03-04 16:59:25 +0300

    Implement document chunking logic using configured splitter

--- services/processor/splitter.py
+++ services/processor/splitter.py
@@ -2,3 +2,7 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 
 def get_text_splitter():
     return RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=50)
+def split_doc(doc_text):
+    splitter = get_text_splitter()
+    return splitter.split_text(doc_text)
+
